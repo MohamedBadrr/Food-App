110710017062473095b8a382d4f5b787f1bb505c
MohamedBadrr <badr59497@gmail.com>
2025-12-25 12:00:23 +0200

    feat: add Events component and related assets for event services

---
+++ app/_components/Events.tsx
@@ -0,0 +1,61 @@
+import Image from "next/image";
+
+const Events = () => {
+  return (
+    <div className=" container py-30 flex gap-15 flex-col ">
+      <h2 className="w-full max-w-168.75 font-medium font-playfair text-[55px]">
+        {" "}
+        We also offer unique services for your events
+      </h2>
+      <div className="grid place-items-center grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
+        {dataEvents.map((event) => (
+          <div
+            className="flex flex-col gap-7 hover:scale-105 transition-all duration-300 "
+            key={event.title}
+          >
+            <div>
+              <Image
+                src={event.imageSrc}
+                alt={`${event.title} image`}
+                width={206}
+                height={120}
+              />
+            </div>
+            <div className="flex flex-col gap-4">
+              <h4 className="text-2xl font-semibold">{event.title}</h4>
+              <p className="text-[11px] w-50">{event.description}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Events;
+const dataEvents = [
+  {
+    title: "Caterings",
+    description:
+      "In the new era of technology we look in the future with certainty for life.",
+    imageSrc: "/assets/category.png",
+  },
+  {
+    title: "Birthdays",
+    description:
+      "In the new era of technology we look in the future with certainty for life.",
+    imageSrc: "/assets/brth.jpg",
+  },
+  {
+    title: "Weddings",
+    description:
+      "In the new era of technology we look in the future with certainty for life.",
+    imageSrc: "/assets/weeding.png",
+  },
+  {
+    title: "Events",
+    description:
+      "In the new era of technology we look in the future with certainty for life.",
+    imageSrc: "/assets/events.png",
+  },
+];
--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Header from "@/components/header";
 import Hero from "./_components/Hero";
 import BrowseOurMenu from "./_components/BrowseOurMenu";
 import ProvideHealthy from "./_components/ProvideHealthy";
+import Events from "./_components/Events";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <Hero />
       <BrowseOurMenu />
       <ProvideHealthy />
+      <Events />
     </main>
   );
 }
