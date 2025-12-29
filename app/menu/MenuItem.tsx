44fddc4eaf1e69bbba85583c4b7ce4eed0c7c538
MohamedBadrr <badr59497@gmail.com>
2025-12-29 13:51:37 +0200

    feat: update MenuItem component styles and add currency formatting utility

--- app/menu/MenuItem.tsx
+++ app/menu/MenuItem.tsx
@@ -10,7 +10,7 @@ const MenuItem = (props: {
   image: string;
 }) => {
   return (
-    <div className="border border-neutral-400 w-full hover-scale hover:shadow-xl hover:bg-primary hover:text-white group md:max-w-76.5 rounded-lg">
+    <div className="border border-neutral-400 w-full  hover:shadow-xl hover:bg-primary hover:text-white group md:max-w-76.5 rounded-lg">
       <Image
         alt="menu image"
         src={props.image}
@@ -18,14 +18,14 @@ const MenuItem = (props: {
         width={230}
         height={306}
       />
-      <div className="flex flex-col items-center justify-center gap-3 p-8">
+      <div className="flex flex-col items-center justify-center gap-2 px-2">
         <p className="font-bold text-[22px] text-primary group-hover:text-white font-playfair">
           $ {props.price}
         </p>
         <p className="italic [22px] font-semibold">{props.title}</p>
         <p className="text-[12px] text-center">{props.description}</p>
         <Button
-          className="rounded-full w-full mt-2 hover:bg-white/90  hover:text-primary group-hover:bg-white group-hover:text-primary  "
+          className="rounded-full w-full my-2 hover:bg-white/90  hover:text-primary group-hover:bg-white group-hover:text-primary  "
           size={"sm"}
         >
           {" "}
